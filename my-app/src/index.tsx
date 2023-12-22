79e396ee202366274202ba0746619d545757d0d2
Rachad <36934217+RachadQ@users.noreply.github.com>
2023-12-21 23:01:01 -0500

    Update App, and index file

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const HelloWorld = () => {
-  return <div>Hello World still</div>;
-};
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-      <HelloWorld />
+    <App />
   </React.StrictMode>
 );
 
